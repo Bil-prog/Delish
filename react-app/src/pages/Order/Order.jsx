08591a056f2e3a197f4b1422252e2d002cbf3347
Bilha Mwengi <bilhamwengi34@gmail.com>
2024-07-26 18:23:18 +0300

    order page

--- react-app/src/pages/Order/Order.jsx
+++ react-app/src/pages/Order/Order.jsx
@@ -1,10 +1,50 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react'
-import './Order'
+import React, {useContext} from 'react'
+import './Order.css'
+import { StoreContext } from "../../context/StoreContext";
 
 const Order = () => {
+  const {getTotalCartAmount} = useContext(StoreContext);
+
   return (
-    <div>Order</div>
+    <form className="place-order">
+      <div className="place-order-left">
+        <p className="title">Delivery Information</p>
+        <div className="multi-fields">
+          <input type="text" placeholder='First name' />
+          <input type="text" placeholder='Last name'/>
+        </div>
+        <input type="email" placeholder='Email address'/>
+        <input type="text" placeholder='Street'/>
+        <div className="multi-fields">
+          <input type="text" placeholder='City' />
+          <input type="text" placeholder='State'/>
+        </div>
+        <input type="text" placeholder='Phone' />
+      </div>
+      <div className="place-order-right">
+      <div className="cart-total">
+          <h2>Cart Totals</h2>
+          <div>
+            <div className="cart-total-details">
+              <p>Subtotal</p>
+              <p>${getTotalCartAmount()}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <p>Delivery Fee</p>
+              <p>${0}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <b><p>Total</p></b>
+              <b><p>${getTotalCartAmount()}</p></b>
+            </div>
+          </div>
+          <button>PROCEED TO PAYMENT</button>
+        </div>
+      </div>
+    </form>
   )
 }
 
